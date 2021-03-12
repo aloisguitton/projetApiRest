e2fbe1798a6fa335bb57cd4a1dc662b6bc63df47
gdorian486 <gueraud8(>
2021-03-12 17:02:44 +0100

    modification route news

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.use('/user', userRouter);
 app.use('/covid', covidRouter);
-app.use('news',newsRouter);
+app.use('/news',newsRouter);
 
 app.use(function(req, res, next) {
     next(createError(404));

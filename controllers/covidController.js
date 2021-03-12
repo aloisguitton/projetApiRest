b0b5c3e47c055bfd2873d19f94f13cac5313ce18
Fortexes <78360660+Fortexes@users.noreply.github.com>
2021-03-12 17:34:33 +0100

    Axios added

--- controllers/covidController.js
+++ controllers/covidController.js
@@ -1,9 +1,5 @@
-exports.getAllUsers = (req, res) => {
-    userModel.getAllUsers()
-    User.findAll().then(
-        user => {
-            console.log(user)
-        }
-    );
-    response.success(res, {message: "data"})
+exports.getAllCountryCovid = (req, res) => {
+
+
+
 }
--- routes/covidRouter.js
+++ routes/covidRouter.js
@@ -2,6 +2,16 @@ let express = require('express');
 let router = express.Router();
 const covidCtrl = require('../controllers/covidController');
 
-//router.get('/allUser', userCtrl.getAllUsers);
+/**
+ * @swagger
+ * /:country :
+ *  get:
+ *   description: Get all country get by covid19api.com
+ *   responses:
+ *    '200':
+ *      description: Return all country
+ *
+ */
+router.get('/:country', covidCtrl.getAllCountryCovid);
 
 module.exports = router;

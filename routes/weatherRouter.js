45ccbb55154ac16e246a67b01b5d826966b7983c
Jérémy Ballan <jeremy@jba.com>
2021-03-16 10:17:48 +0100

    Gestion de la base de donnée weather

--- routes/weatherRouter.js
+++ routes/weatherRouter.js
@@ -2,6 +2,11 @@ let express = require('express');
 let router = express.Router();
 const weatherCtrl = require('../controllers/weatherController');
 
+/**
+ * @swagger
+ * /weather/getweather:
+ */
+
 router.get('/getweather', weatherCtrl.getAllWeather);
 
 module.exports = router;

3e41134a08ef7f2383cf6e9104d90640ff0d40dc
aloisguitton <aloisguitton@orange.fr>
2021-03-31 13:50:03 +0200

    server

---
+++ server/models/userModel.js
@@ -0,0 +1,108 @@
+const sequelize = require("sequelize");
+const {User, Weather} =  require("./db");
+const jwt = require('jsonwebtoken');
+const cryptoRandomString = require('crypto-random-string');
+const CryptoJS = require('crypto-js');
+
+exports.createToken = async (token) => {
+    const res = {
+        token: jwt.sign(
+            {token: token},
+            'uZY8+a4#dqs7B___enseirbbbbbbbbbb___j^QBk2F9093:z',
+            {expiresIn: '31d'}
+        )
+    }
+    return res['token']
+}
+
+exports.register = (firstName, lastName, pass, email, city, zip, addr) => {
+    return new Promise((resolve, reject) => {
+        User.create({
+            fist_name: firstName,
+            last_name: lastName,
+            password: pass,
+            email: email,
+            address: addr,
+            postal_code: zip,
+            city: city,
+            token: CryptoJS.MD5(cryptoRandomString({length: 25, type: 'alphanumeric'})).toString()
+        })
+            .then(() => {
+                resolve()
+            })
+            .catch(() => {
+                reject()
+            })
+    })
+}
+
+exports.connect = (email, password) => {
+    return new Promise((resolve, reject) => {
+        User.findAll({
+            attributes: [
+                [sequelize.fn('COUNT', sequelize.col('token')), 'cnt'],
+                'token'
+            ],
+            where: {email: email, password: password}})
+            .then((res) => {
+                let result;
+                let data = res[0]['dataValues'];
+                if(data['cnt'] === 0){
+                    result = false
+                } else {
+                    result = data['token']
+                }
+                resolve(result)
+            })
+            .catch((err) => {
+                reject()
+            })
+    })
+}
+
+exports.retrieveId = (token) => {
+    return new Promise((resolve, reject) => {
+        User.findAll({
+            attributes: [
+                'id'
+            ],
+            where: {token: token}})
+            .then((res) => {
+                console.log(res)
+                resolve(res[0]['id'])
+            })
+            .catch((err) => {
+                reject()
+            })
+    })
+}
+
+exports.weatherParameters = (user) => {
+    return new Promise((resolve, reject) => {
+        Weather.findAll({
+            attributes: [
+                [sequelize.fn('COUNT', sequelize.col('id')), 'cnt'],
+                'city_name',
+                'units',
+                'language',
+            ],
+            where: {user_id: user}})
+            .then((res) => {
+                let result;
+                let data = res[0]['dataValues'];
+                if(data['cnt'] === 0){
+                    result = false
+                } else {
+                    result = {
+                        'city_name': data['city_name'],
+                        'units': data['units'],
+                        'language': data['language'],
+                    }
+                }
+                resolve(result)
+            })
+            .catch((err) => {
+                reject()
+            })
+    })
+}
---
+++ server/routes/userRouter.js
@@ -0,0 +1,104 @@
+let express = require('express');
+let router = express.Router();
+
+const userCtrl = require('../controllers/userController');
+const auth = require('../middleware/auth')
+
+/**
+ * @swagger
+ * /user/register:
+ *  post:
+ *   description: Get all users
+ *   responses:
+ *    '200':
+ *      description: Return all api users
+ *
+ *   parameters:
+ *    - in: body
+ *      name: user
+ *      schema:
+ *       type: object
+ *       required:
+ *        - firstname
+ *        - lastname
+ *        - password
+ *        - email
+ *        - address
+ *        - city
+ *        - zip
+ *       properties:
+ *        firstname:
+ *         type: string
+ *        lastname:
+ *         type: string
+ *        password:
+ *         type: string
+ *         example: "1a1dc91c907325c69271ddf0c944bc72"
+ *        email:
+ *         type: string
+ *        address:
+ *         type: string
+ *        city:
+ *         type: string
+ *        zip:
+ *         type: string
+ */
+router.post('/register', userCtrl.register)
+
+/**
+ * @swagger
+ * /user/connect:
+ *  post:
+ *   description: Get all users
+ *   responses:
+ *    '200':
+ *      description: Connection route
+ *    '500':
+ *      description: An error occured
+ *    '401':
+ *      description: Incorrect email or password
+ *
+ *   parameters:
+ *    - in: body
+ *      name: id
+ *      schema:
+ *       type: object
+ *       required:
+ *        - email
+ *        - password
+ *       properties:
+ *        email:
+ *         type: string
+ *         example: "test@gmail.fr"
+ *        password:
+ *         type: string
+ *         example: "1a1dc91c907325c69271ddf0c944bc72"
+ */
+router.post('/connect', userCtrl.connect)
+
+/**
+ * @swagger
+ * /user/parameters:
+ *  get:
+ *   description: Get all users
+ *   responses:
+ *    '200':
+ *      description: Informations
+ *    '500':
+ *      description: An error occured
+ *    '401':
+ *      description: Unauthorized
+ *
+ *   parameters:
+ *    - in: query
+ *      name: token
+ *      type: string
+ *      description: "token from connect route"
+ *    - in: query
+ *      name: userId
+ *      type: string
+ *      description: "userId from connect route"
+ */
+router.get('/parameters', auth, userCtrl.parameters)
+
+module.exports = router;
